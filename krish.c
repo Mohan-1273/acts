94f68026afca9d5e708ed6abf7a828f0dd7323c9
mohan <krishnakrish1273@gmail.com>
2019-11-06 16:34:16 +0530

    hiiiii

---
+++ krish.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+ double num,root;
+printf("enter the num value:");
+scanf("%lf",&num);
+root=sqrt(num);
+printf("squareroot of %.2lf = %.2lf:",num,root);
+return 0;
+}
+
